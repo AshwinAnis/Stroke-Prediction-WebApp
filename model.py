58ab063a19c09b7ab5e2945b23bad0bb682af39b
Ashwin Anis <ashwinanis1@gmail.com>
2021-12-03 17:35:44 +0400

    Oversampling using SMOTE

--- model.py
+++ model.py
@@ -15,8 +15,13 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.preprocessing import OneHotEncoder
 
 
+# Data Augmentation
+from imblearn.over_sampling import SMOTE
+
+
 # Pipeline
 
+from imblearn.pipeline import make_pipeline
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.compose import make_column_transformer
@@ -44,7 +49,7 @@ sd = stroke_data.dropna()
 sd = sd.reset_index()
 y = sd['stroke']
 X = sd[['gender','age','hypertension','heart_disease','ever_married','work_type','Residence_type','avg_glucose_level','bmi','smoking_status']]
-# X_train,X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state=42)
+
 
 # Splitting Columns into Countinuous and Caetegorical 
 
@@ -57,14 +62,15 @@ categorical_transformer = OneHotEncoder(sparse=False, handle_unknown='ignore')
 
 preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, num_cols),('cat', categorical_transformer, cat_cols)], remainder='drop', n_jobs=1)
 
-clf = Pipeline(steps=[('preprocessor', preprocessor),
- ('classifier', LogisticRegression(random_state=100))])
+# Create the pipeline
+pipeline = make_pipeline(preprocessor, SMOTE(), LogisticRegression(random_state=100))
 
 
-clf.fit(X, y)
+# Fit Data
+pipeline.fit(X, y)
 
 #Save model
-pickle.dump(clf, open('model.pkl','wb'))
+pickle.dump(pipeline, open('model.pkl','wb'))
 
 # Loading Model
 model  = pickle.load(open('model.pkl','rb'))
